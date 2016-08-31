0f11be93f5dc13073d692d811cb6eee58e612217
ezadm <admin@ezadm.in>
2016-08-31 10:28:32 +0100

    fixed spelling mistake in tos url

--- boilerplate.sh
+++ boilerplate.sh
@@ -40,7 +40,7 @@ show_ezadmin_header() #{{{
   echo ''
   echo ''
   echo 'By using this script you agree to the EZADM.IN TOS.'
-  echo 'The Terms of Service can be found at https://www.exadm.in/tos'
+  echo 'The Terms of Service can be found at https://www.ezadm.in/tos'
   echo ''
   echo 'p.s. Please report any bugs to https://bugs.ezadm.in.'
   echo ''
