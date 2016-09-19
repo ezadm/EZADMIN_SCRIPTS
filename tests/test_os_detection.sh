b7e5af3406fb825f6d36ed189e13de245b8ca64b
Alan James Jenkins <alan.james.jenkins@gmail.com>
2016-09-19 09:53:09 +0100

    Manually ran the tests on ubuntu already but will continue to work on this for automated testing.

--- tests/test_os_detection.sh
+++ tests/test_os_detection.sh
@@ -3,7 +3,7 @@
 # ubuntu
 UBUNTU_VERSIONS=( 12.04.5 14.04.5 16.04 16.10 )
 for ubuntuversion in ${UBUNTU_VERSIONS[@]}; do
-    docker run -at --rm --name $CUR_DISTRIB_ezadmin --volume=.:/ezadmin:ro bash /ezadmin/run_os_detect.sh
+    docker run -a --rm --name $CUR_DISTRIB_ezadmin --volume=$PWD:/ezadmin:ro bash /ezadmin/run_os_detect.sh
 done
 
 # arch
