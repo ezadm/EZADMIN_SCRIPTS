21fb32c11943cfddfbc9981823fec00e5c10ea98
Alan James Jenkins <alan.james.jenkins@gmail.com>
2016-10-10 12:58:09 +0100

    Add the site migrator script.

---
+++ sitemigrator.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+###### BEGIN BOILERPLATE ########{{{
+ezadmin_include() #{{{
+{
+    INCLUDEURL="$1"
+
+    if [ -x /usr/bin/curl ]; then
+        curl $INCLUDEURL -so /tmp/ezadmin_include.sh
+    elif [ -x /usr/bin/wget ]; then
+        wget $INCLUDEURL -qO /tmp/ezadmin_include.sh
+    else
+        echo "Neither wget or curl found. Unable to continue, aborting EZADMIN."
+        echo "Please install wget or curl and then try again."
+    fi
+    source /tmp/ezadmin_include.sh
+    rm -f /tmp/ezadmin_include.sh
+} #}}}
+ezadmin_include 'https://raw.githubusercontent.com/demon012/EZADMIN_SCRIPTS/master/boilerplate.sh'
+###### END BOILERPLATE ########}}}
+
+generate_ctrlpanel_username()#{{{
+{
+    export CTRLPANEL_USERNAME=`echo "$DOMAIN" | sed 's/\.//' | grep -Eo '^.{0,16}'`
+    ezadmin_message "Using control panel username ${CTRLPANEL_USERNAME}"
+}#}}}
+generate_ctrlpanel_password()#{{{
+{
+    export CTRLPANEL_PASSWORD=`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-16};echo;`
+    ezadmin_message "Using control panel password ${CTRLPANEL_PASSWORD}"
+}#}}}
+
+# create hosting account
+create_hosting_account()#{{{
+{
+    generate_ctrlpanel_username
+    generate_ctrlpanel_password
+    if [ "$EZADMIN_CTRLPANEL" == "plesk" ]; then
+        CREATE_ACCOUNT_CMD="plesk bin subscription --create ${DOMAIN} -owner admin -service-plan 'Default Domain'"
+        ezadmin_message "Creating Plesk account with command:"
+        ezadmin_message "${CREATE_ACCOUNT_CMD}"
+
+        export ALLDEST=/var/www/vhosts/${DOMACCOUNT}_ALLFILES/
+        export SITEDEST=/var/www/vhosts/${DOMACCOUNT}/httpdocs/
+    elif [ "$EZADMIN_CTRLPANEL" == "cpanel" ]; then
+        CREATE_ACCOUNT_CMD="/scripts/wwwacct ${DOMAIN} ${CTRLPANEL_USERNAME} ${CTRLPANEL_PASSWORD} x3 n n n 0 0 0 0 0 0"
+        ezadmin_message "Creating CPanel account with command:"
+        ezadmin_message "${CREATE_ACCOUNT_CMD}"
+        ${CREATE_ACCOUNT_CMD}
+
+        export DOMACCOUNT=`cat /etc/userdomains | grep "$DOMAIN" | cut -d' ' -f 2`
+        export ALLDEST=/var/www/vhosts/${DOMACCOUNT}_ALLFILES/
+        export SITEDEST=/var/www/vhosts/${DOMACCOUNT}/httpdocs/
+    elif [ "$EZADMIN_CTRLPANEL" == "unknown" ]; then
+        ezadmin_message_error "Could not detect control panel. Unable to migrate the site."
+        exit
+    fi
+}#}}}
+
+# migrate files
+migrate_files()#{{{
+{
+    mkdir $ALLDEST
+    cd $ALLDEST
+    # check if rsync is available
+    # ssh -p $SRC_SSHPORT $SRC_SSHUSER@$SRC_SSH "which rsync"
+    # if rsync available use rsync
+    # if [ $? -eq 0 ]; then
+    #     rsync -avzp --progress $SRC_SSHPORT $SRC_SSHUSER@$SRC_SSH:
+    # else
+    # else fallback to tarsync
+
+    ssh -p $SRC_SSHPORT $SRC_SSHUSER@$SRC_SSH 'tar cvpj .' | tar xvpj
+    cp -a $ALLDEST/htdocs/* $SITEDEST
+
+    # fi
+}#}}}
+
+# identify site type
+identify_site_cms()#{{{
+{
+}#}}}
+
+# parse site config file
+parse_site_cms_config()#{{{
+{
+}#}}}
+
+# create database
+create_site_database()#{{{
+{
+}#}}}
+
+# create database user
+create_site_database_user()#{{{
+{
+}#}}}
+
+# grant database user permissions
+grant_database_permissions()#{{{
+{
+}#}}}
+
+# migrate database
+migrate_database()#{{{
+{
+}#}}}
+
+# update site config file
+update_cms_config()#{{{
+{
+}#}}}
+
+# fix site file permissions
+fix_site_file_permissions()
+{
+    if [ "$EZADMIN_CTRLPANEL" == "cpanel" ]; then
+        wget https://raw.githubusercontent.com/PeachFlame/cPanel-fixperms/master/fixperms.sh && bash fixperms.sh -a $DOMACCOUNT && rm -f fixperms.sh
+    elif [ "$EZADMIN_CTRLPANEL" == "plesk" ]; then
+        /usr/local/psa/bin/repair --restore-vhosts-permissions
+    else
+        ezadmin_message_error "Command to fix permissions is unknown."
+    fi
+}
