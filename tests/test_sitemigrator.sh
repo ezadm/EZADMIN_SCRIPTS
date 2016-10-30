56fafec09ba10abe8dfb580855bad2a4aa11adf5
Alan James Jenkins <alan.james.jenkins@gmail.com>
2016-10-30 08:42:12 +0000

    Make all the tests fail until they have been implemented.

--- tests/test_sitemigrator.sh
+++ tests/test_sitemigrator.sh
@@ -2,71 +2,88 @@
 
 test_validate_input()
 {
+    fail "validate_input is untested"
 }
 
 test_generate_ctrlpanel_username()
 {
+    fail "generate_ctrlpanel_username is untested"
 }
 
 test_generate_ctrlpanel_password()
 {
+    fail "generate_ctrlpanel_password is untested"
 }
 
 test_create_hosting_account()
 {
+    fail "create_hosting_account is untested"
 }
 
 test_init_variables()
 {
+    fail "init_variables is untested"
 }
 
 test_migrate_files()
 {
+    fail "migrate_files is untested"
 }
 
 test_fix_site_file_permissions()
 {
+    fail "fix_site_file_permissions is untested"
 }
 
 test_identify_site_cms()
 {
+    fail "identify_site_cms is untested"
 }
 
 test_parse_site_cms_config()
 {
+    fail "parse_site_cms_config is untested"
 }
 
 test_verify_db_credentials()
 {
+    fail "verify_db_credentials is untested"
 }
 
 test_escape_database_details()
 {
+    fail "escape_database_details is untested"
 }
 
 test_create_site_database()
 {
+    fail "create_site_database is untested"
 }
 
 test_create_site_database_user()
 {
+    fail "create_site_database_user is untested"
 }
 
 test_grant_database_permissions()
 {
+    fail "grant_database_permissions is untested"
 }
 
 test_migrate_database()
 {
+    fail "migrate_database is untested"
 }
 
 test_update_cms_config()
 {
+    fail "update_cms_config is untested"
 }
 
 test_sitemigrator_options()
 {
+    fail "sitemigrator_options is untested"
 }
 
 # load shunit2
-../shunit2/src/shunit2
+source ../shunit2/src/shunit2
